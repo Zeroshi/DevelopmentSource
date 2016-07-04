a538a493e135d01e30d517a8f1d913903aa5f2bd
zeroshi <thomasvnelson@live.com>
2016-07-03 23:01:01 -0400

    Added Reverse string extension and test

---
+++ Extensions/Entensions/Entensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeroshi.Entensions
+{
+    public static class StringExtensions
+    {
+        public static string AppendTest(this string text)
+        {
+            return string.Format("Test-{0}", text);
+        }
+
+        public static string Reverse(this string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+    }
+}
---
+++ Extensions/Entensions/ExtensionTests/ExtensionUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeroshi.Entensions;
+
+namespace ExtensionTests
+{
+    [TestClass]
+    public class ExtensionUnitTests
+    {
+        [TestMethod]
+        public void TestMeAppendTest()
+        {
+            var test = "I am a file";
+            var expected = "Test-I am a file";
+            var actual = test.AppendTest();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReverseTest()
+        {
+            var test = "abcd";
+            var expected = "dcba";
+            var actual = test.Reverse();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
