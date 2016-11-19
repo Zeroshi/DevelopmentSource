fd1370e2ad96a82f56a5f9a3581581889e347614
zeroshi <thomasvnelson@live.com>
2016-11-19 18:05:33 -0500

    added additional extensions for strings and their tests

--- Extensions/Entensions/Entensions/StringExtensions.cs
+++ Extensions/Entensions/Entensions/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Zeroshi.Entensions
@@ -25,7 +26,7 @@ namespace Zeroshi.Entensions
                 return null;
             }
 
-            char[] array = text.ToCharArray();
+            var array = text.ToCharArray();
             Array.Reverse(array);
             return new String(array);
         }
@@ -37,10 +38,13 @@ namespace Zeroshi.Entensions
                 return null;
             }
 
-            char[] textArray = text.ToCharArray();
+            var textArray = text.ToCharArray();
             var usedIndexes = new List<int>();
             var randomText = string.Empty;
             var random = new Random();
+            var builder = new StringBuilder();
+
+            builder.Append(randomText);
 
             do
             {
@@ -48,13 +52,70 @@ namespace Zeroshi.Entensions
 
                 if (!usedIndexes.Contains(randomIndex))
                 {
-                    randomText += textArray[randomIndex];
+                    builder.Append(textArray[randomIndex]);
                     usedIndexes.Add(randomIndex);
                 }
 
             } while (randomText.Length < text.Length);
+            randomText = builder.ToString();
 
             return randomText;
+        }
+
+        public static string RemoveNonNumerics(this string text)
+        {
+            return Regex.Replace(text, @"\D", "");
+        }
+
+        public static string RemoveSpecialCharacters(this string text)
+        {
+            return Regex.Replace(text, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
+        }
+
+        public static bool ContainsNumerics(this string text)
+        {
+            return text.Any(char.IsDigit);
+        }
+
+        public static bool ContainsAlphaNumbericCharacters(this string text)
+        {
+            return text.Any(char.IsLetterOrDigit);
+        }
+
+        public static bool ContainsAlphaCharacters(this string text)
+        {
+            return text.Any(char.IsLetter);
+        }
+
+        public static string AddSpacesToCamelCase(this string text)
+        {
+            var updatedString = string.Empty;
+
+            //insert spaces before each capitolized character
+            if (!string.IsNullOrEmpty(text) && text.Length >= 0)
+            {
+                foreach (var character in text)
+                {
+                    if (character.ToString().Any(char.IsUpper))
+                    {
+                        updatedString += string.Format(@"{0}{1}", " ", character);
+                    }
+                    else
+                    {
+                        updatedString += character;
+                    }
+                }
+            }
+
+            //Capitolize the initial charater in the new string 
+            if (updatedString.Length > 0)
+            {
+                var startUpper = updatedString[0].ToString().ToUpper();
+                updatedString = updatedString.Remove(0, 1);
+                updatedString = updatedString.Insert(0, startUpper);                
+            }
+
+            return updatedString;
         }
     }
 }
--- Extensions/Entensions/ExtensionTests/ExtensionUnitTests.cs
+++ Extensions/Entensions/ExtensionTests/ExtensionUnitTests.cs
@@ -27,14 +27,87 @@ namespace ExtensionTests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void RandomOutputTest()
-        {
-            var test = "abcd dfsg ts";
-            var expected = "dcba dfsg ts";
-            var actual = test.RandomOutput();
+        //[TestMethod]
+        //public void RandomOutputTest()
+        //{
+        //    var test = "abcd dfsg ts";
+        //    var expected = "dcba dfsg ts";
+        //    var actual = test.RandomOutput();
 
-            Assert.IsTrue(expected.Length == actual.Length);
+        //    Assert.IsTrue(expected.Length == actual.Length);
+        //}
+
+        [TestMethod]
+        public void RemoveNonNumerics()
+        {
+            var test = "abcd1 dfsg2 ts3";
+            var expected = "123";
+            var actual = test.RemoveNonNumerics();
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void RemoveNonAlpaNumerics()
+        {
+            var test = "a1b2!#@(*c3";
+            var expected = "a1b2c3";
+            var actual = test.RemoveSpecialCharacters();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ContainsNumerics()
+        {
+            var test = "abc";
+            var expected = false;
+            var actual = test.ContainsNumerics();
+
+            Assert.IsTrue(expected == actual);
+
+            test = "abc7";
+            expected = true;
+            actual = test.ContainsNumerics();
+        }
+
+        [TestMethod]
+        public void ContainsAlphaCharacters()
+        {
+            var test = "abc";
+            var expected = true;
+            var actual = test.ContainsAlphaCharacters();
+
+            Assert.IsTrue(expected == actual);
+
+            test = "*&^325";
+            expected = false;
+            actual = test.ContainsNumerics();
+        }
+
+
+        [TestMethod]
+        public void ContainsAlphaNumerics()
+        {
+            var test = "*&%()*&!";
+            var expected = false;
+            var actual = test.ContainsAlphaNumbericCharacters();
+
+            Assert.IsTrue(expected == actual);
+
+            test = "abc7&^";
+            expected = true;
+            actual = test.ContainsNumerics();
+        }
+
+        [TestMethod]
+        public void AddSpacesToCamelCase()
+        {
+            var test = "thisIsATest";
+            var expected = "This Is A Test";
+            var actual = test.AddSpacesToCamelCase();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
